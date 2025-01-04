7b183b40a52524ec771d36806681ed63bf5800e1
manoj <ommanoj921@gmail.com>
2025-01-04 23:14:06 +0530

    adding WeatherResponse

---
+++ src/main/java/com/example/weatherapp/dto/WeatherResponse.java
@@ -0,0 +1,33 @@
+package com.example.weatherapp.dto;
+
+public class WeatherResponse {
+
+    private double temperature;
+    private int humidity;
+    private String weatherDescription;
+
+    // Getters and Setters
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getWeatherDescription() {
+        return weatherDescription;
+    }
+
+    public void setWeatherDescription(String weatherDescription) {
+        this.weatherDescription = weatherDescription;
+    }
+}
